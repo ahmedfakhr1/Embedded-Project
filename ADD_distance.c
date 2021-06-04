afc0717d61a11a90e93864f3264bdaf2a84e3ace
omaralzayat <66072168+omaralzayat@users.noreply.github.com>
2021-06-04 15:42:54 +0200

    Create ADD_distance.c

---
+++ ADD_distance.c
@@ -0,0 +1,13 @@
+int  ADD_distance(int dist, int* olddist){
+	dist += *olddist;
+	*olddist = dist;
+	return dist;
+}
+
+int main(){
+  while ( distance < 100){
+		distance = dist(36.12, -86.67 , 33.94, -118.4);
+		distance = ADD_distance( distance, &olddist);
+	}
+
+}
