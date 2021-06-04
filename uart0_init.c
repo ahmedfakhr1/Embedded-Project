a2ff678218b667d271c3a1789e6f6faaad3ca02f
ahmedfakhr1 <84821705+ahmedfakhr1@users.noreply.github.com>
2021-06-04 02:02:45 +0200

    Update uart0_init.c

--- uart0_init.c
+++ uart0_init.c
@@ -1,28 +1,31 @@
 #include <tm4c123gh6pm.h>
 
 void uart0init(void){
-SYSCTL_RCGCGPIO_R|=0X01; //turning port a on
+SYSCTL_RCGCGPIO_R |=0X00000001; //turning port a on
+SYSCTL_RCGCUART_R |=0x00000001;
 while ((SYSCTL_PRGPIO_R&0X01) != 1){ };
 
 
-//gpio a settings
-GPIO_PORTA_DEN_R |= 0xFF;
-GPIO_PORTA_AMSEL_R&=~0xFF;
-GPIO_PORTA_DIR_R |= 0x02 & (~01);
-GPIO_PORTA_LOCK_R = 0x4C4F434B;
-GPIO_PORTA_CR_R |= 0xFF;
-GPIO_PORTA_AFSEL_R |=0x03;
-GPIO_PORTA_PCTL_R |=0x11;
+
 
 
 //uart settings
 UART0_CTL_R &= ~0x00000001; //disable
 //settings
 UART0_IBRD_R = 104;  //div is 104.1666667
-UART0_FBRD_R = 0x2AAA;
-UART0_LCRH_R |=0xB0; //fifo and word length of 8 bits
+UART0_FBRD_R = 11;
+UART0_LCRH_R |=0x70; //fifo and word length of 8 bits
 //enable
 UART0_CTL_R |= (1<<8) | (1<<9);
 UART0_CTL_R |=(1);
-//GPIO settings
+
+
+//gpio a settings
+GPIO_PORTA_DEN_R |= 0xFF;
+GPIO_PORTA_AMSEL_R&=~0xFF;
+GPIO_PORTA_DIR_R |= 0x02 & (~0x01);
+GPIO_PORTA_LOCK_R = 0x4C4F434B;
+GPIO_PORTA_CR_R |= 0xFF;
+GPIO_PORTA_AFSEL_R |=0x03;
+GPIO_PORTA_PCTL_R |=0x11&(~0xFFFFFF00);
 }
