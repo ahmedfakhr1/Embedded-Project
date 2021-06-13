e36bba8460dbb033c755cb7d1c8cab53ab698ed1
Mamdouh Ibrahim Youssef Mohamed <mamdouhmekky123@gmail.com>
2021-06-13 13:00:07 +0200

    Update longitude Averaging

--- longitudeAvg.c
+++ longitudeAvg.c
@@ -1,3 +1,10 @@
+
+/*This code for getting the average of the longitudes 
+we have assign a global variable called (freq) so that we
+could divide the sum of numbers oflongitudes on it 
+so that we could reduce the error
+*/
+
 double averaging_lon(double x )
 {
 static double total = 0;
@@ -5,20 +12,14 @@ static int n1 = 0;
 double res = 0;
 total +=x;
 n1++;
-if (n1==freq)
+if (n1==freq)// freq is a global variable
 {
 
 res = total /n1;
 	total=0;
 	n1 =0;
-	return res ;
+	return res ; // res --> the result after getting the average of the longetudes 
 	
 }
-else
-{
 	return res;
-}
-
-	
-	
 }
